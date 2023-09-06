5f4f1ae0591fed3ef9880dc3097c97ee91d9dff3
prachihirekha <prachihirekhan32@gmail.com>
2023-09-06 14:48:30 +0530

    DAS Arraylist practice

---
+++ src/main/java/dsa/arraylist/ArrayListFloat.java
@@ -0,0 +1,20 @@
+package dsa.arraylist;
+
+import java.util.ArrayList;
+
+public class ArrayListFloat {
+    public static void main(String[] args) {
+        //ArrayList<Float>list= new ArrayList<>();
+//        list.add(1.3f);
+//        list.add(12.35f);
+//        list.add(234.343465f);
+//        list.add(234.67f);
+//        System.out.println(list);
+        // bye using for loop
+
+        float[] arr={1,2,3,3,45};
+        for (float i:arr) {
+            System.out.println(i);
+        }
+    }
+}
